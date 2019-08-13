bab172474faab065be57a740c2f996eaa2e672d6
Schneider, Yves (IPD) <yves.schneider@kit.edu>
2019-08-13 10:42:41 +0200

    First basic structure for the senders and receivers

--- src/main/java/edu/kit/ipd/are/esda/EnergyStateDataAnalysis/EnergyStateDataAnalysisApplication.java
+++ src/main/java/edu/kit/ipd/are/esda/EnergyStateDataAnalysis/EnergyStateDataAnalysisApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EnergyStateDataAnalysisApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EnergyStateDataAnalysisApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EnergyStateDataAnalysisApplication.class, args);
+    }
 
 }
--- src/test/java/edu/kit/ipd/are/esda/EnergyStateDataAnalysis/EnergyStateDataAnalysisApplicationTests.java
+++ src/test/java/edu/kit/ipd/are/esda/EnergyStateDataAnalysis/EnergyStateDataAnalysisApplicationTests.java
@@ -1,16 +1,35 @@
 package edu.kit.ipd.are.esda.EnergyStateDataAnalysis;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import edu.kit.ipd.are.esda.consumer.Receiver;
+import edu.kit.ipd.are.esda.producer.Sender;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+// @RunWith(SpringRunner.class)
+// @SpringBootTest
+// @DirtiesContext
+// @EmbeddedKafka(partitions = 1, topics =
+// {EnergyStateDataAnalysisApplicationTests.HELLOWORLD_TOPIC})
 public class EnergyStateDataAnalysisApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    // static final String HELLOWORLD_TOPIC = "helloworld.t";
+    //
+    // @Autowired
+    // Receiver receiver;
+    //
+    // @Autowired
+    // Sender sender;
+    //
+    // @Test
+    // public void contextLoads() {
+    // assertTrue("receiver", receiver != null);
+    // assertTrue("sender", sender != null);
+    // }
 
 }
